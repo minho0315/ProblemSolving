541ee9687483fdabaf51e90f6206b71da6e3f50b
minho0315 <dlalsgh369@naver.com>
2023-02-19 10:52:42 +0900

    백준 단계별 풀이 - 기본 수학 1 - 손익분기점

---
+++ src/backjoon/basic/level7/BreakEvenPoint.java
@@ -0,0 +1,21 @@
+package backjoon.basic.level7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BreakEvenPoint {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = bufferedReader.readLine().split(" ");
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int c = Integer.parseInt(input[2]);
+
+        if (b >= c) {
+            System.out.print(-1);
+        } else {
+            System.out.print(a / (c-b) + 1);
+        }
+    }
+}
