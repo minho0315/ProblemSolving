c3a582000c6f844b3c567ce095a49122b365da24
minho0315 <dlalsgh369@naver.com>
2023-03-07 00:41:16 +0900

    백준 - 알고리즘 기초 1/2 - 후위 표기식

--- src/backjoon/basic1/datastructure2/PostfixNotation.java
+++ src/backjoon/basic1/datastructure2/PostfixNotation.java
@@ -3,39 +3,43 @@ package backjoon.basic1.datastructure2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Stack;
 
 public class PostfixNotation {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
-        List<String> alpha = new ArrayList<>();
-        Stack<String> stack = new Stack<>();
-        char[] input = bufferedReader.readLine().toCharArray();
-
-        for (int i = 0; i < n; i++) {
-            alpha.add(bufferedReader.readLine());
-        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        char[] input = br.readLine().toCharArray();
 
         for (int i = 0; i < input.length; i++) {
             if (input[i] >= 'A' && input[i] <='Z') {
-                stack.push(alpha.get(input[i] - 'A'));
+                sb.append(input[i]);
             } else {
-                double s2= Double.parseDouble(stack.pop());
-                double s1= Double.parseDouble(stack.pop());
-                if (input[i] == '+') {
-                    stack.push(String.valueOf(s1 + s2));
-                } else if (input[i] == '-') {
-                    stack.push(String.valueOf(s1 - s2));
-                } else if (input[i] == '*') {
-                    stack.push(String.valueOf(s1 * s2));
-                } else if (input[i] == '/') {
-                    stack.push(String.valueOf(s1 / s2));
+                if (input[i] == '(') {
+                    stack.add(input[i]);
+                } else if (input[i] == '*' || input[i] == '/') {
+                    while (!stack.empty() && (stack.peek() == '*' || stack.peek() == '/')) {
+                        sb.append(stack.pop());
+                    }
+                    stack.add(input[i]);
+                } else if (input[i] == '+' || input[i] == '-') {
+                    while (!stack.empty() && stack.peek() != '(') {
+                        sb.append(stack.pop());
+                    }
+                    stack.add(input[i]);
+                } else if (input[i] == ')') {
+                    while (!stack.empty() && stack.peek() != '(') {
+                        sb.append(stack.pop());
+                    }
+                    stack.pop();
                 }
             }
         }
-
-        System.out.printf("%.2f", Double.parseDouble(stack.peek()));
+        while (!stack.empty()) {
+            sb.append(stack.pop());
+        }
+        System.out.println(sb);
     }
 }
-//1935 후위 표기식2
+//1918 후위 표기식
---
+++ src/backjoon/basic1/datastructure2/PostfixNotation2.java
@@ -0,0 +1,41 @@
+package backjoon.basic1.datastructure2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class PostfixNotation2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        List<String> alpha = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        char[] input = bufferedReader.readLine().toCharArray();
+
+        for (int i = 0; i < n; i++) {
+            alpha.add(bufferedReader.readLine());
+        }
+
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] >= 'A' && input[i] <='Z') {
+                stack.push(alpha.get(input[i] - 'A'));
+            } else {
+                double s2= Double.parseDouble(stack.pop());
+                double s1= Double.parseDouble(stack.pop());
+                if (input[i] == '+') {
+                    stack.push(String.valueOf(s1 + s2));
+                } else if (input[i] == '-') {
+                    stack.push(String.valueOf(s1 - s2));
+                } else if (input[i] == '*') {
+                    stack.push(String.valueOf(s1 * s2));
+                } else if (input[i] == '/') {
+                    stack.push(String.valueOf(s1 / s2));
+                }
+            }
+        }
+
+        System.out.printf("%.2f", Double.parseDouble(stack.peek()));
+    }
+}
+//1935 후위 표기식2
