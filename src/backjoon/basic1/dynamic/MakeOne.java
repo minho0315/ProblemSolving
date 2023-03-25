8861d4721e9c81270d1d32499cd948f7b9d7fe36
minho0315 <dlalsgh369@naver.com>
2023-03-25 12:20:59 +0900

    백준 - 알고리즘 기초 1/2 - 1로만들기 (BFS)

--- src/backjoon/basic1/dynamic/MakeOne.java
+++ src/backjoon/basic1/dynamic/MakeOne.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class MakeOne {
 //    https://bio-info.tistory.com/159
@@ -28,33 +30,80 @@ public class MakeOne {
 //        System.out.println(d[x]);
 //    }
 
-    private static HashMap<Integer, Integer> dp = new HashMap<>() {{
-        put(1, 0);
-    }};
+//    private static HashMap<Integer, Integer> dp = new HashMap<>() {{
+//        put(1, 0);
+//    }};
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int x = Integer.parseInt(br.readLine());
+//        System.out.println(rec(x));
+//    }
+//
+//    private static int rec(int n) {
+//
+//        if (dp.keySet().contains(n)) {
+//            return dp.get(n);
+//        }
+//
+//        if (n%3 == 0 && n%2 == 0) {
+//            dp.put(n, Math.min(rec(n/2) + 1, rec(n/3) + 1));
+//        } else if (n % 3 == 0) {
+//            dp.put(n, Math.min(rec(n/3) + 1, rec(n-1) + 1));
+//        } else if (n % 2 == 0) {
+//            dp.put(n, Math.min(rec(n/2) + 1, rec(n-1) + 1));
+//        } else {
+//            dp.put(n, rec(n-1) + 1);
+//        }
+//
+//        return dp.get(n);
+//    }
 
+    //BFS 풀이
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int x = Integer.parseInt(br.readLine());
-        System.out.println(rec(x));
-    }
+        Queue<Integer> q = new LinkedList<>();
+        q.add(x);
+        int[] visited = new int[x + 1];
 
-    private static int rec(int n) {
-
-        if (dp.keySet().contains(n)) {
-            return dp.get(n);
+        while (q.size() != 0) {
+            int c = q.remove();
+            if (c == 1) {
+                break;
+            }
+            if (c % 3 == 0 && visited[c/3] == 0) {
+                q.add(c/3);
+                visited[c/3] = visited[c] + 1;
+            }
+            if (c % 2 == 0 && visited[c/2] == 0) {
+                q.add(c/2);
+                visited[c/2] = visited[c] + 1;
+            }
+            if (visited[c-1] == 0) {
+                q.add(c-1);
+                visited[c-1] = visited[c] + 1;
+            }
         }
-
-        if (n%3 == 0 && n%2 == 0) {
-            dp.put(n, Math.min(rec(n/2) + 1, rec(n/3) + 1));
-        } else if (n % 3 == 0) {
-            dp.put(n, Math.min(rec(n/3) + 1, rec(n-1) + 1));
-        } else if (n % 2 == 0) {
-            dp.put(n, Math.min(rec(n/2) + 1, rec(n-1) + 1));
-        } else {
-            dp.put(n, rec(n-1) + 1);
-        }
-
-        return dp.get(n);
+        System.out.println(visited[1]);
     }
 }
 //1463 1로 만들기
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
