7f46236546579347c886d0ccc64649c11d5c105c
minho0315 <dlalsgh369@naver.com>
2023-04-03 08:55:27 +0900

    백준 - 알고리즘 기초 1/2 - 다이나믹 프로그래밍 - 연속합 복습

--- src/backjoon/basic1/dynamic/ContinuousSum.java
+++ src/backjoon/basic1/dynamic/ContinuousSum.java
@@ -3,8 +3,31 @@ package backjoon.basic1.dynamic;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class ContinuousSum {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(br.readLine());
+//        int[] arr = new int[n];
+//        int[] dp = new int[n];
+//        String[] input = br.readLine().split(" ");
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = Integer.parseInt(input[i]);
+//        }
+//
+//        dp[0] = arr[0];
+//        int max = arr[0];
+//
+//        for (int i = 1; i < n; i++) {
+//            dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
+//            max = Math.max(max, dp[i]);
+//        }
+//
+//        System.out.println(max);
+//
+//    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -16,14 +39,12 @@ public class ContinuousSum {
         }
 
         dp[0] = arr[0];
-        int max = arr[0];
 
         for (int i = 1; i < n; i++) {
             dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
-            max = Math.max(max, dp[i]);
         }
 
-        System.out.println(max);
+        System.out.println(Arrays.stream(dp).max().getAsInt());
 
     }
 }
