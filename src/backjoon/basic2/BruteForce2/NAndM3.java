dd9dd50bdd3474621038ed81e1c6ffe54fd6fa41
minho0315 <dlalsgh369@naver.com>
2023-05-16 08:57:18 +0900

    백준 - 알고리즘 기초 2/2 - 브루트 포스 - N과 M (3)

---
+++ src/backjoon/basic2/BruteForce2/NAndM3.java
@@ -0,0 +1,43 @@
+package backjoon.basic2.BruteForce2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NAndM3 {
+
+    public static int[] arr;
+    public static int N, M;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth) {
+
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            arr[depth] = i;
+            dfs(depth + 1);
+        }
+    }
+}
+//15651 N과 M (3)
