eba064be11b47f21164874e0f001dd8a6c47e365
minho0315 <dlalsgh369@naver.com>
2023-05-17 08:50:35 +0900

    백준 - 알고리즘 기초 2/2 - 브루트 포스 - N과 M (4)

--- src/backjoon/basic2/BruteForce2/NAndM4.java
+++ src/backjoon/basic2/BruteForce2/NAndM4.java
@@ -5,40 +5,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class NAndM3 {
+public class NAndM4 {
 
-    public static int[] arr;
-    public static int N, M;
-    public static StringBuilder sb = new StringBuilder();
+    private static int[] arr;
+    private static int N, M;
+    private static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
         arr = new int[M];
-        dfs(0);
+
+        dfs(1, 0);
         System.out.println(sb);
     }
 
-    public static void dfs(int depth) {
+    private static void dfs(int at, int depth) {
 
         if (depth == M) {
-            for (int i = 0; i < M; i++) {
-                sb.append(arr[i]).append(' ');
+            for (int val : arr) {
+                sb.append(val).append(' ');
             }
             sb.append('\n');
             return;
         }
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = at; i <= N; i++) {
             arr[depth] = i;
-            dfs(depth + 1);
+            dfs(i, depth + 1);
         }
     }
 }
-//15651 N과 M (3)
-//https://st-lab.tistory.com/116
+//15652 N과 M (4)
+//https://st-lab.tistory.com/117
